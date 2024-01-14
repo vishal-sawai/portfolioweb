71797e1d1a127439952452a926d4607ccfe24e25
vishal sawai <vishalsawai1725@gmail.com>
2024-01-15 00:27:33 +0530

    Remove unnecessary code from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,30 @@ import {
   Route,
 } from "react-router-dom";
 import ProjectInfo from './components/ProjectInfo';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500)
+  })
+
   return (
 
-    <>
+    <>{isLoading ? (
+      <Loader />
+    ) : (
       <Routes>
         <Route exact path='/' element={<Main />} />
         <Route path='/home' element={<Main />} />
         <Route path='/projectinfopage/:PName' element={<ProjectInfo />} />
       </Routes>
-
+    )}
     </>
   );
 }
