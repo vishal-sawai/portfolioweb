4f85cefec7bb8c047c1a492b126e6ba46b1e477d
vishal sawai <vishalsawai1725@gmail.com>
2024-01-16 15:00:41 +0530

    Update ParticleBg component to use @tsparticles/react library

--- src/components/ParticleBg.jsx
+++ src/components/ParticleBg.jsx
@@ -1,18 +1,119 @@
 import React from 'react'
-import ParticleConfig from './Config/ParticleConfig';
-// import Particles from 'react-tsparticles';
-// import { loadFull } from 'tsparticles';
+import { useCallback, useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 
 export default function ParticleBg() {
 
-    // const particlesInit = (engine) => {
-    //     loadFull(engine);
-    // };
+    const [init, setInit] = useState(false);
 
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
     return (
         <div id='Home'>
-            {/* <Particles init={particlesInit} options={ParticleConfig}></Particles> */}
+            {init && (
+                <Particles
+                    id="tsparticles"
+
+                    particlesLoaded={particlesLoaded}
+                    options={{
+                        background: {
+                            color: {
+                                // value: "#0d47a1",
+                            },
+                        },
+                        fullScreen: {
+                            enable: false
+                        },
+                        // fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onClick: {
+                                    enable: true,
+                                    mode: "push",
+                                },
+                                onHover: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 4,
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4,
+                                },
+                            },
+                        },
+                        particles: {
+                            "number": {
+                                "value": 100,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 400
+                                }
+                            },
+                            "color": {
+                                "value": "#ffffff"
+                            },
+                            links: {
+                                color: "#ffffff",
+                                distance: 150,
+                                enable: true,
+                                opacity: 0.4,
+                                width: 1,
+                            },
+                            "move": {
+                                "enable": true,
+                                "speed": 6,
+                                "direction": "none",
+                                "random": false,
+                                "straight": false,
+                                "out_mode": "out",
+                                "bounce": false,
+                                "attract": {
+                                    "enable": false,
+                                    "rotateX": 600,
+                                    "rotateY": 1200
+                                }
+                            },
+                            "opacity": {
+                                "value": 0.5,
+                                "random": false,
+                            },
+                            "shape": {
+                                "type": "circle",
+                                "stroke": {
+                                    "width": 0,
+                                    "color": "#000000"
+                                },
+                                "polygon": {
+                                    "nb_sides": 5
+                                },
+                            },
+                            size: {
+                                "value": 3,
+                                "random": true,
+                            },
+                        },
+                        detectRetina: true,
+                    }}
+                />
+            )}
         </div>
     );
+
 }
